d6a0a6ae317ac988a0511e7a0748ebe9ed18d2a4
Sitanans <11144sitanan@gmail.com>
2023-04-20 23:45:06 +0700

    signup_and_login

--- BeSiMoProject/storefront/accounts/models.py
+++ BeSiMoProject/storefront/accounts/models.py
@@ -1,39 +1,29 @@
 from django.db import models
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.core.validators import RegexValidator, EmailValidator
 
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)   
-    # telephone_number = models.CharField(
-    #     max_length=17, 
-    #     validators=[
-    #         RegexValidator(
-    #             regex=r'^\+?1?\d{9,15}$',
-    #             message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-    #         )
-    #     ]
-    # )
-    # email = models.EmailField(validators=[EmailValidator()])
+    telephone_number = models.CharField(
+        max_length=17,
+        validators=[
+            RegexValidator(
+                regex=r'^(\+66|0)[1-9]\d{8}$',
+                message="Phone number must be entered in the format: '+66999999999' or '0999999999'."
+            )
+        ]
+    )
+    email = models.EmailField(validators=[EmailValidator()])
 
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
 
-# class Buyer(forms.Form):
-#     name = forms.CharField(max_length=255)
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     address = forms.CharField(max_length=255)
-#     telephone_number = forms.CharField(max_length=17, validators=[RegexValidator(
-#             regex=r'^\+?1?\d{9,15}$',
-#             message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-#         )])
-#     email = forms.EmailField(validators=[EmailValidator()])
+
 
 
--- BeSiMoProject/storefront/accounts/urls.py
+++ BeSiMoProject/storefront/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('success/', views.success, name='success'),
+    path('login/', views.login_view, name='login')
 ]
