0a8f482f7ee71238d36b29593aac3d38d86cccf1
Sitanans <11144sitanan@gmail.com>
2023-04-21 19:37:40 +0700

    signup_and_login
    but cant put in database

--- BeSiMoProject/storefront/accounts/models.py
+++ BeSiMoProject/storefront/accounts/models.py
@@ -1,29 +1,31 @@
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.core.validators import RegexValidator, EmailValidator
-
+import re
 
 class Buyer(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
+    phone_regex = RegexValidator(
+        regex=r'^(\+66|0)\d{9}$',
+        message="Phone number must be in the format '0xxxxxxxxx' or '+66xxxxxxxxx'")
+    phone = models.CharField(validators=[phone_regex], max_length=10)
+    email = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)   
-    telephone_number = models.CharField(
-        max_length=17,
-        validators=[
-            RegexValidator(
-                regex=r'^(\+66|0)[1-9]\d{8}$',
-                message="Phone number must be entered in the format: '+66999999999' or '0999999999'."
-            )
-        ]
-    )
-    email = models.EmailField(validators=[EmailValidator()])
+    password_again = models.CharField(max_length=255, verbose_name='Password confirmation')
 
+    def clean(self):
+        if self.password and self.password_again and self.password != self.password_again:
+            raise ValidationError('Passwords do not match')
+
+        self.clean_email()
+
+    def clean_email(self):
+        email = self.email
+        if not re.match(r'^\d{10}@student\.chula\.ac\.th$', email):
+            raise ValidationError('Email must be a valid Chula email address')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
-
 
 
--- BeSiMoProject/storefront/accounts/views.py
+++ BeSiMoProject/storefront/accounts/views.py
@@ -1,16 +1,19 @@
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from django.contrib.auth import login
+from .forms import BuyerForm
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = BuyerForm(request.POST)
         if form.is_valid():
-            # save the form data to the database here
-            return redirect('success')  # redirect to success view
+            user = form.save()
+            login(request, user)
+            return redirect('success')
     else:
-        form = SignupForm()
+        form = BuyerForm()
     return render(request, 'signup.html', {'form': form})
 
 
@@ -19,25 +22,18 @@ def success(request):
     return render(request, 'success.html')
 
 
-
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            # Get the username and password from the form
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-
-            # Authenticate the user
             user = authenticate(request, username=username, password=password)
             if user is not None:
-                # Log the user in and redirect to the home page
                 login(request, user)
                 return redirect('home')
             else:
-                # Invalid login credentials
                 form.add_error(None, 'Invalid username or password')
     else:
         form = AuthenticationForm()
-
     return render(request, 'login.html', {'form': form})
