8f30477f071543b0098d08b380a0073f15eabb72
Yosita Puengsiricharoen <6341191826@cbs.chula.ac.th>
2023-05-03 16:12:06 +0700

    Revert "editedversion3/5"
    This reverts commit 4976bf8f2160b24bea19e213f812f180ab03ba56.

--- BeSiMoProject/storefront/accounts/admin.py
+++ BeSiMoProject/storefront/accounts/admin.py
@@ -1,32 +1,9 @@
-# from django.contrib import admin
-
-# from .models import *
-
-# # Register your models here.
-
-# admin.site.register(Buyer)
-# admin.site.register(Category)
-# admin.site.register(Product)
-
 from django.contrib import admin
-from .models import Products
-from .models import Category
-from .models import Customer
-from .models import Order
 
-
-class AdminProduct(admin.ModelAdmin):
-    list_display = ['name', 'price', 'category']
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+from .models import *
 
 # Register your models here.
-admin.site.register(Products,AdminProduct)
+
+admin.site.register(Buyer)
 admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Order)
-
-
-# username = Tanushree, email = tanushree7252@gmail.com, password = 1234
+admin.site.register(Product)
--- BeSiMoProject/storefront/accounts/apps.py
+++ BeSiMoProject/storefront/accounts/apps.py
@@ -1,12 +1,6 @@
-# from django.apps import AppConfig
-
-# class AccountsConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'accounts'
-
 from django.apps import AppConfig
 
-class StoreConfig(AppConfig):
+
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
