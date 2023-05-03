22771afd7456dca75b6c05eea325141b7a16622e
Yosita Puengsiricharoen <6341191826@cbs.chula.ac.th>
2023-05-03 16:42:01 +0700

    edit2 3/5 4pm

--- BeSiMoProject/storefront/accounts/models.py
+++ BeSiMoProject/storefront/accounts/models.py
@@ -14,7 +14,6 @@ class Category(models.Model):
         return self.name
         
 
-<<<<<<< Updated upstream
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -28,22 +27,6 @@ class Product(models.Model):
     
     @property
     def imageURL(self):
-=======
-class Customer(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField (max_length=50)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-
-    #to save the data
-    def register(self):
-        self.save()
-
-
-    @staticmethod
-    def get_customer_by_email(email):
->>>>>>> Stashed changes
         try:
             return Customer.objects.get(email=email)
         except:
--- BeSiMoProject/storefront/accounts/urls.py
+++ BeSiMoProject/storefront/accounts/urls.py
@@ -11,15 +11,8 @@ urlpatterns = [
     path('success/', views.success, name='success'),
     path('login/', views.login_view, name='login'),
     path('category_browse/', views.category_browse, name='category_browse'),
-<<<<<<< Updated upstream
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('category_browse/<int:category_id>/', views.product_browse, name='product_browse')
     # path('product_browse/', views.product_browse, name='product_browse')
-=======
-    path('category_browse/<int:category_id>/', views.product_browse, name='product_browse'),
-    path('store', views.homepage, name='homepage'),
-    path('buyer/', views.category_browse, name='buyer'),
-    path('seller/', views.seller_signup, name='seller'),
->>>>>>> Stashed changes
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
