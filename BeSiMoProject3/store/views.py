afde42c34603fcbd009dae0324659cce79462a1b
Yosita Puengsiricharoen <6341191826@cbs.chula.ac.th>
2023-05-06 15:45:25 +0700

    ลองsignupแต่ยังerrorของbuyer

--- BeSiMoProject3/store/views.py
+++ BeSiMoProject3/store/views.py
@@ -127,51 +127,93 @@ def orders(request):
     return render(request, 'orders.html', {'orders': orders})
 
 
+# def signup(request):
+#     if request.method == 'POST':
+#         first_name = request.POST.get('firstname')
+#         last_name = request.POST.get('lastname')
+#         phone = request.POST.get('phone')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+
+#         # validation
+#         error_message = None
+#         if not first_name:
+#             error_message = "Please Enter your First Name !!"
+#         elif len(first_name) < 3:
+#             error_message = 'First Name must be 3 char long or more'
+#         elif not last_name:
+#             error_message = 'Please Enter your Last Name'
+#         elif len(last_name) < 3:
+#             error_message = 'Last Name must be 3 char long or more'
+#         elif not phone:
+#             error_message = 'Enter your Phone Number'
+#         elif len(phone) < 10:
+#             error_message = 'Phone Number must be 10 char Long'
+#         elif len(password) < 5:
+#             error_message = 'Password must be 5 char long'
+#         elif len(email) < 5:
+#             error_message = 'Email must be 5 char long',
+#         elif not re.match(r'^\d{10}\@student\.chula\.ac\.th$', email):
+#             error_message = 'Email must be in the format: 6xxxxxxxxx@student.chula.ac.th'
+#         elif Customer.objects.filter(email=email).exists():
+#             error_message = 'Email Address Already Registered..'
+#         else:
+#             customer = Customer(first_name=first_name,
+#                                 last_name=last_name,
+#                                 phone=phone,
+#                                 email=email,
+#                                 password=make_password(password))
+#             customer.save()
+#             return redirect('homepage')
+
+#         data = {
+#             'error': error_message,
+#             'values': request.POST
+#         }
+#         return render(request, 'signup.html', data)
+
+#     return render(request, 'signup.html')
+
+
 def signup(request):
     if request.method == 'POST':
-        first_name = request.POST.get('firstname')
-        last_name = request.POST.get('lastname')
-        phone = request.POST.get('phone')
+        # Get form data
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
-
-        # validation
-        error_message = None
-        if not first_name:
-            error_message = "Please Enter your First Name !!"
-        elif len(first_name) < 3:
-            error_message = 'First Name must be 3 char long or more'
-        elif not last_name:
-            error_message = 'Please Enter your Last Name'
-        elif len(last_name) < 3:
-            error_message = 'Last Name must be 3 char long or more'
-        elif not phone:
-            error_message = 'Enter your Phone Number'
-        elif len(phone) < 10:
-            error_message = 'Phone Number must be 10 char Long'
-        elif len(password) < 5:
-            error_message = 'Password must be 5 char long'
-        elif len(email) < 5:
-            error_message = 'Email must be 5 char long',
-        elif not re.match(r'^\d{10}\@student\.chula\.ac\.th$', email):
-            error_message = 'Email must be in the format: 6xxxxxxxxx@student.chula.ac.th'
-        elif Customer.objects.filter(email=email).exists():
-            error_message = 'Email Address Already Registered..'
-        else:
-            customer = Customer(first_name=first_name,
-                                last_name=last_name,
-                                phone=phone,
-                                email=email,
-                                password=make_password(password))
-            customer.save()
-            return redirect('homepage')
-
-        data = {
-            'error': error_message,
-            'values': request.POST
-        }
-        return render(request, 'signup.html', data)
-
+        phone = request.POST.get('phone')
+        
+       
+        # Validate email format
+        email_regex = re.compile(r'^\d{9,11}@student\.chula\.ac\.th$')
+        if not email_regex.match(email):
+            raise ValidationError({'email': 'Email must be in the format: \'xxxxxxxxxx@student.chula.ac.th\''})
+        
+        # Check if email already exists
+        if User.objects.filter(email=email).exists():
+            return HttpResponse('Email already exists')
+        
+        user = User.objects.create_user(username=email, password=password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+       
+        
+        # Create Customer object
+        customer = Customer.objects.create(
+            created_by=user,
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            email=email,
+            password=password
+        )
+        customer.save()
+        # Redirect to home page
+        return redirect('homepage')
+    
+    # Render signup form
     return render(request, 'signup.html')
 
 
