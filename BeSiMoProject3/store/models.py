61cff41136fafda743ff72334f4feea0de752f17
Yosita Puengsiricharoen <6341191826@cbs.chula.ac.th>
2023-05-06 15:31:47 +0700

    ของสิตาcreatbycustomer+แก้แสดงผลหน้าadmin

--- BeSiMoProject3/store/models.py
+++ BeSiMoProject3/store/models.py
@@ -17,14 +17,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-# class Customer(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField (max_length=50)
-#     phone = models.CharField(max_length=10)
-#     email=models.EmailField()
-#     password = models.CharField(max_length=100)
 
 class Customer(models.Model):
+    created_by = models.OneToOneField(User, related_name='buyer', on_delete=models.CASCADE,)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
@@ -32,30 +27,23 @@ class Customer(models.Model):
     phone_regex = RegexValidator(
         regex=r'^(\+66|0)\d{9}$',
         message="Phone number must be in the format '0xxxxxxxxx' or '+66xxxxxxxxx'")
-    phone = models.CharField(validators=[phone_regex], max_length=10)
+    phone = models.CharField(validators=[phone_regex], max_length=12)
     
     email_regex = re.compile(r'^\d{9,11}@student\.chula\.ac\.th$')
     email = models.EmailField(unique=True)
+    
 
     def clean(self):
         if not self.email_regex.match(self.email):
             raise ValidationError({'email': 'Email must be in the format: \'xxxxxxxxxx@student.chula.ac.th\''})
 
-        if not validate_email(self.email):
-            raise ValidationError({'email': 'Please enter a valid email address'})
-
-    # email_regex = RegexValidator(
-    #     regex=r'^\d{9,11}@student.chula.ac.th$',
-    #     message="Email must be in the format: '6xxxxxxxxx@student.chula.ac.th'")
-    # email = models.EmailField(validators=[email_regex], unique=True, error_messages={'invalid': 'Please enter a valid email address'})
-
-    #to save the data
+  
     def register(self):
         self.save()
 
 
     @staticmethod
-    def get_customer_by_email(email):
+    def get_seller_by_email(email):
         try:
             return Customer.objects.get(email= email)
         except:
@@ -67,6 +55,66 @@ class Customer(models.Model):
             return True
 
         return False
+    
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+    
+    
+# class Customer(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField (max_length=50)
+#     phone = models.CharField(max_length=10)
+#     email=models.EmailField()
+#     password = models.CharField(max_length=100)
+
+# class Customer(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     password = models.CharField(max_length=100)
+    
+#     phone_regex = RegexValidator(
+#         regex=r'^(\+66|0)\d{9}$',
+#         message="Phone number must be in the format '0xxxxxxxxx' or '+66xxxxxxxxx'")
+#     phone = models.CharField(validators=[phone_regex], max_length=10)
+    
+#     email_regex = re.compile(r'^\d{9,11}@student\.chula\.ac\.th$')
+#     email = models.EmailField(unique=True)
+
+#     def clean(self):
+#         if not self.email_regex.match(self.email):
+#             raise ValidationError({'email': 'Email must be in the format: \'xxxxxxxxxx@student.chula.ac.th\''})
+
+#         # if not validate_email(self.email):
+#         #     raise ValidationError({'email': 'Please enter a valid email address'})
+
+#     # email_regex = RegexValidator(
+#     #     regex=r'^\d{9,11}@student.chula.ac.th$',
+#     #     message="Email must be in the format: '6xxxxxxxxx@student.chula.ac.th'")
+#     # email = models.EmailField(validators=[email_regex], unique=True, error_messages={'invalid': 'Please enter a valid email address'})
+
+#     #to save the data
+#     def register(self):
+#         self.save()
+
+
+#     @staticmethod
+#     def get_customer_by_email(email):
+#         try:
+#             return Customer.objects.get(email= email)
+#         except:
+#             return False
+
+
+#     def isExists(self):
+#         if Customer.objects.filter(email = self.email):
+#             return True
+
+#         return False
+    
+#     def __str__(self):
+#         return self.first_name + " " + self.last_name
     
 class Store(models.Model):
     store_name = models.CharField(max_length=100, default='')    
